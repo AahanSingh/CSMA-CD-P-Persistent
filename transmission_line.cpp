1553d599c2b4b15ed366285e29e3b6e1a4b12e72
akhilraj95 <akhil95raj@gmail.com>
2015-04-16 01:16:56 +0530

    collision in transmission channel updated, detection of flag by each node,reading to the read stack of each node -COMPLETED

--- transmission_line.cpp
+++ transmission_line.cpp
@@ -1,3 +1,7 @@
+/*
+	NOTE:  If you are debugging a logical error in the collision mechanism, its most probably the section (@line 400)
+
+*/
 #include<iostream>
 #include<queue>
 using namespace std;
@@ -12,6 +16,7 @@ int arrayD[25];
 int arrayE[25];
 int arrayF[25];
 
+
 int index_A=0;
 int index_B=4;
 int index_C=9;
@@ -20,14 +25,135 @@ int index_E=18;
 int index_F=24;
 
 std::vector<int> send_stack[6];
+deque<int> read_queue[6];
 
 int status[6]={0,0,0,0,0,0};		//listen=0   read=1   send= 2   jam=3
 
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////FRAME ENCODE/DECODE/////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*	NODE ADDRESS  
+ * A-000
+ * B-001
+ * C-010
+ * D-011
+ * E-100
+ * F-101
+  */
+vector<int> setFrame(vector<int> data,int rec) //rec=decimal value
+{
+    vector<int> frame;
+    //adding flag 01111110
+    frame.push_back(0);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(0);
+    int count=0;
+    //adding receiver address
+
+    switch(rec)
+    {
+        case 0:
+            frame.push_back(0);
+            frame.push_back(0);
+            frame.push_back(0);
+            break;
+        case 1:
+            frame.push_back(0);
+            frame.push_back(0);
+            frame.push_back(1);
+            break;
+        case 2:
+            frame.push_back(0);
+            frame.push_back(1);
+            frame.push_back(0);
+            break;
+        case 3:
+            frame.push_back(0);
+            frame.push_back(1);
+            frame.push_back(1);
+            break;
+        case 4:
+            frame.push_back(1);
+            frame.push_back(0);
+            frame.push_back(0);
+            break;
+        case 5:
+            frame.push_back(1);
+            frame.push_back(0);
+            frame.push_back(1);
+            break;
+    }
+
+    //adding data
+    for(int i=0;i<data.size();i++)
+    {
+        int x=data[i];
+        if(x==1)
+            count++;
+        else
+            count=0;
+        frame.push_back(x);
+        if(count==5)
+        {
+            frame.push_back(0);
+            count=0;
+        }
+    }
+
+    //adding flag 01111110
+    frame.push_back(0);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(1);
+    frame.push_back(0);
+    vector<int> copy;
+    //reversing the frame
+    for(int i=frame.size()-1;i>=0;i--)
+    {
+        int x=frame[i];
+        copy.push_back(x);
+    }
+    cout<<"\n";
+    return copy;
+      //return frame;
+}
+
+int decode(vector<int> frame, int rec)
+{
+    int len=frame.size();
+    int cmp=0;
+    for(int i=8;i<=10;i++)
+    {
+        cmp*=10;
+        cmp+=frame[i];
+    }
+    cout<<cmp<<"\n";
+
+    for(int i=11;i<=frame.size()-9;i++)
+        cout<<frame[i];
+    cout<<"\n";
+    return 0;
+}
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 
 void next_step()
 {		
 		int x=1;		//del
+		
+
 		while(x!=0)
 		{	
 			cout<<"Do you want to send message(0/1)";
@@ -53,13 +179,14 @@ void next_step()
 					cin>>y;
 				}while(y!=-1);
 
+				xframe=setFrame(xdata,xreceiver);
 				//frame(xdata,xreceiver)		
 									//del
-				int arr[4]={2,0,1,1};
-				for(int i=0;i<4;i++)
-					xframe.push_back(arr[i]);
-					
-				for(int i=0;i<4;i++)
+				//int arr[4]={2,0,1,1};
+				//for(int i=0;i<4;i++)
+				//	xframe.push_back(arr[i]);
+				cout<<"\nThe frame to be sent:";
+				for(int i=0;i<xframe.size();i++)
 					cout<<xframe[i];
 
 				status[xsender]=2;
@@ -95,13 +222,13 @@ void next_step()
 
 			for(int i=0;i<index_D;i++)		//shift  D
 				arrayD[i]=arrayD[i+1];
-			for(int i=24;i>index_A;i--)
+			for(int i=24;i>index_D;i--)
 				arrayD[i]=arrayD[i-1];
 			arrayD[index_D]=0;
 
 			for(int i=0;i<index_E;i++)		//shift  E
 				arrayE[i]=arrayE[i+1];
-			for(int i=24;i>index_A;i--)
+			for(int i=24;i>index_E;i--)
 				arrayE[i]=arrayE[i-1];
 			arrayE[index_E]=0;
 
@@ -110,7 +237,7 @@ void next_step()
 			arrayF[index_F]=0;	
 
 
-			if(status[0]==2)
+			if(status[0]==2)										//pushing send stack or 0
 				{	if(send_stack[0].empty())
 					   status[0]=0;
 					else
@@ -127,7 +254,7 @@ void next_step()
 				arrayA[index_A]=0;
 			
 			
-			if(status[1]==2)
+			if(status[1]==2)									//pushing send stack or 0
 				{	
 					if(send_stack[1].empty())
 					   status[1]=0;
@@ -141,7 +268,7 @@ void next_step()
 			else
 				arrayB[index_B]=0;
 			
-			if(status[2]==2)
+			if(status[2]==2)											//pushing send stack or 0
 				{
 					if(send_stack[2].empty())
 					   status[2]=0;
@@ -155,7 +282,7 @@ void next_step()
 			else
 				arrayC[index_C]=0;
 			
-			if(status[3]==2)
+			if(status[3]==2)														//pushing send stack or 0
 				{	if(send_stack[3].empty())
 					   status[3]=0;
 					else
@@ -168,7 +295,7 @@ void next_step()
 			else
 				arrayD[index_D]=0;
 			
-			if(status[4]==2)
+			if(status[4]==2)													//pushing send stack or 0
 				{	if(send_stack[4].empty())
 					   status[4]=0;
 					else
@@ -181,7 +308,7 @@ void next_step()
 			else
 				arrayE[index_E]=0;
 			
-			if(status[5]==2)
+			if(status[5]==2)													//pushing send stack or 0
 				{	if(send_stack[5].empty())
 					   status[5]=0;
 					else
@@ -193,56 +320,286 @@ void next_step()
 				}
 			else
 				arrayF[index_F]=0;
+//-----------------------------------------------------------------------------------------------			
+											//addition of the node arrays
+
+			for(int i=0;i<25;i++)
+				{
+					transmission_line[i]=arrayA[i]+arrayB[i]+arrayC[i]+arrayD[i]+arrayE[i]+arrayF[i];
+				}
+//----------------------------------------------------------------------------------------------
 				
 //************************************************************************************************
+																				//Displaying the node arrays and transmission line
 			int j;
-			cout<<"array A printing"<<endl;
+			cout<<"\narray A printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayA[j];
 				
 			}
 
-			cout<<"array B printing"<<endl;
+			cout<<"\narray B printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayB[j];
 				
 			}
 
-			cout<<"array C printing"<<endl;
+			cout<<"\narray C printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayC[j];
 				
 			}
 
-			cout<<"array D printing"<<endl;
+			cout<<"\narray D printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayD[j];
 				
 			}
 
-			cout<<"array E printing"<<endl;
+			cout<<"\narray E printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayE[j];
 				
 			}
 
-			cout<<"array F printing"<<endl;
+			cout<<"\narray F printing";
 			for(j=0;j<25;j++)
 			{
 				cout<<arrayF[j];
 				
 			}
 
+			cout<<"\nTrans line     -";
+			for(j=0;j<25;j++)
+			{
+				cout<<transmission_line[j];
+				
+			}
+
 			
-//************************************************************************************************			
-//-----------------------------------------------------------------------------------------------			
-											//addition of the node arrays
-//----------------------------------------------------------------------------------------------
+//************************************************************************************************		
+
+//----------------------------------------------------------------------------------------------------
+										//updating the node arrays with abnormal transmission line signals
+			
+			for (int i = 0; i < 25; ++i)
+			{
+			if(transmission_line[i]>1)	
+				{if((status[0]==2) &&(arrayA[i]>=1))
+					arrayA[i]=2;
+
+				if((status[1]==2) &&(arrayB[i]>=1))
+					arrayB[i]=2;
+				
+				if((status[2]==2) &&(arrayC[i]>=1))
+					arrayC[i]=2;
+
+				if((status[3]==2) &&(arrayD[i]>=1))
+					arrayD[i]=2;
+
+				if((status[4]==2) &&(arrayE[i]>=1))
+					arrayE[i]=2;
+
+				if((status[5]==2) &&(arrayF[i]>=1))
+					arrayF[i]=2;
+				}
+				
+				
+				
+			}
+			
+//----------------------------------------------------------------------------------------------------	
+//----------------------------------------------------------------------------------------------------
+																//updating the read stack 
+			if(status[0]==0 || status[0]==1)
+				read_queue[0].push_back(transmission_line[index_A]);
+			if(status[1]==0 || status[1]==1)
+				read_queue[1].push_back(transmission_line[index_B]);
+			if(status[2]==0 || status[2]==1)
+				read_queue[2].push_back(transmission_line[index_C]);
+			if(status[3]==0 || status[3]==1)
+				read_queue[3].push_back(transmission_line[index_D]);
+			if(status[4]==0 || status[4]==1)
+				read_queue[4].push_back(transmission_line[index_E]);
+			if(status[5]==0 || status[5]==1)
+				read_queue[5].push_back(transmission_line[index_F]);
+
+//----------------------------------------------------------------------------------------------------
+
+//----------------------------------------------------------------------------------------------------
+							//CLEARING THE READSTACK IF IT HAS SIZE >8 and has no 111111
+							//ELSE SETTING MODE TO READ 
+			if((read_queue[0].size()==8)&&(status[0]==0))
+				{	int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[0][i]!=1)
+							{read_queue[0].pop_front();
+							  break;
+							}
+					}
+					if(i==7)
+					 status[0]=1;
+
+				}
+			if((read_queue[1].size()==8)&&(status[1]==0))
+				{	int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[1][i]!=1)
+							{read_queue[1].pop_front();
+							  break;
+							}
+					}
+					if(i==7)
+					 status[1]=1;
+				}
+			if((read_queue[2].size()==8)&&(status[2]==0))
+				{int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[2][i]!=1)
+							{read_queue[2].pop_front();
+							 break;
+							}
+					}
+					if(i==7)
+					 status[2]=1;
+
+				}
+			if((read_queue[3].size()==8)&&(status[3]==0))
+				{int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[3][i]!=1)
+							{read_queue[3].pop_front();
+							 break;
+							}
+					}
+					if(i==7)
+					 status[3]=1;
+
+				}
+			if((read_queue[4].size()==8)&&(status[4]==0))
+				{int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[4][i]!=1)
+							{read_queue[4].pop_front();
+							 break;
+							}
+					}
+					if(i==7)
+					 status[4]=1;
+
+				}
+			if((read_queue[5].size()==8)&&(status[5]==0))
+				{int i;
+					for( i=1;i<7;i++)
+					{	if(read_queue[5][i]!=1)
+							{read_queue[5].pop_front();
+							 break;
+							}
+					}
+					if(i==7)
+					 status[5]=1;
+
+				}
+//----------------------------------------------------------------------------------------------------
+//----------------------------------------------------------------------------------------------------
+											//in read mode read till you get the last few bits as 1111110
+											//if so decode
+			if(status[0]==1 && read_queue[0].size()>9)
+				{	int i;
+					for( i=read_queue[0].size()-2;i>read_queue[0].size()-8;i--)
+					{
+						if(read_queue[0][i]!=1)
+								break;	
+					}
+					if(i==read_queue[0].size()-8)
+						{cout<<"FRAME RECIEVED AT 0";
+						 while(read_queue[0].size())
+							read_queue[0].pop_front();
+						 status[0]=0;
+						}
+				}
+
+		
+			if(status[1]==1 && read_queue[1].size()>9)
+				{	int i;
+					for( i=read_queue[1].size()-2;i>read_queue[1].size()-8;i--)
+					{
+						if(read_queue[1][i]!=1)
+								break;	
+					}
+					if(i==read_queue[1].size()-8)
+						{cout<<"FRAME RECIEVED AT 1";
+						 while(read_queue[1].size())
+							read_queue[1].pop_front();
+						status[1]=0;
+						}
+				}
+
+			if(status[2]==1 && read_queue[2].size()>9)
+				{	int i;
+					for( i=read_queue[2].size()-2;i>read_queue[2].size()-8;i--)
+					{
+						if(read_queue[2][i]!=1)
+								break;	
+					}
+					if(i==read_queue[2].size()-8)
+						{cout<<"FRAME RECIEVED AT 2";
+						 while(read_queue[2].size())
+							read_queue[2].pop_front();
+						status[2]=0;
+						}
+				}
+
+			if(status[3]==1 && read_queue[3].size()>9)
+				{	int i;
+					for( i=read_queue[3].size()-2;i>read_queue[3].size()-8;i--)
+					{
+						if(read_queue[3][i]!=1)
+								break;	
+					}
+					if(i==read_queue[3].size()-8)
+						{cout<<"FRAME RECIEVED AT 3";
+						 while(read_queue[3].size())
+							read_queue[3].pop_front();
+						 status[3]=0;
+						}
+				}
+			
+			if(status[4]==1 && read_queue[4].size()>9)
+				{	int i;
+					for( i=read_queue[4].size()-2;i>read_queue[4].size()-8;i--)
+					{
+						if(read_queue[4][i]!=1)
+								break;	
+					}
+					if(i==read_queue[4].size()-8)
+						{cout<<"FRAME RECIEVED AT 4";
+						 while(read_queue[4].size())
+							read_queue[4].pop_front();
+						 status[4]=0;
+						}
+				}
+
+			if(status[5]==1 && read_queue[5].size()>9)
+				{	int i;
+					for( i=read_queue[5].size()-2;i>read_queue[5].size()-8;i--)
+					{
+						if(read_queue[5][i]!=1)
+								break;	
+					}
+					if(i==read_queue[5].size()-8)
+						{cout<<"FRAME RECIEVED AT 5";
+						 while(read_queue[5].size())
+							read_queue[5].pop_front();
+						 status[5]=0;
+						}
+				}
+//----------------------------------------------------------------------------------------------------
 
 
 
@@ -278,6 +635,8 @@ int main()
 
 	for(int i=0;i<25;i++)
 		transmission_line[i]=0;
+	
+	
 
 	next_step();
 	return 0;	
